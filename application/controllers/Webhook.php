421b6b8ead5af44c9908a61145993d6fbc8a5661
Toni Haryanto <toha.samba@gmail.com>
2017-05-18 14:30:42 +0700

    add model

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -10,31 +10,63 @@ use \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
 
 class Webhook extends CI_Controller {
 
-  private $bot;
-  private $events;
-  private $signature; 
-  private $user;
+	private $bot;
+	private $events;
+	private $signature; 
+	private $user;
 
-  function __construct()
-  {
-    parent::__construct();
-    $this->load->model('tebakkode_m');
-  }
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('tebakkode_m');
+	}
 
-  public function index()
-  {
-    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-      echo "Hello Coders!";
-      header('HTTP/1.1 400 Only POST method allowed');
-      exit;
-    } 
+	public function index()
+	{
+		if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+			echo "Hello Coders!";
+			header('HTTP/1.1 400 Only POST method allowed');
+			exit;
+		} 
 
-    // get request
-    $body = file_get_contents('php://input');
-    $this->signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE']) ? $_SERVER['HTTP_X_LINE_SIGNATURE'] : "-";
-    $this->events = json_decode($body, true);
+		// get request
+		$body = file_get_contents('php://input');
+		$this->signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE']) ? $_SERVER['HTTP_X_LINE_SIGNATURE'] : "-";
+		$this->events = json_decode($body, true);
 
-    // log every event requests);
-    $this->tebakkode_m->log_events($this->signature, $body);
-  }
+		// log every event requests);
+		$this->tebakkode_m->log_events($this->signature, $body);
+
+		// create bot object
+		$httpClient = new CurlHTTPClient($_SERVER['CHANNEL_ACCESS_TOKEN']);
+		$bot  = new LINEBot($httpClient, ['channelSecret' => $_SERVER['CHANNEL_SECRET']]);
+
+		foreach ($this->events['events'] as $event)
+		{
+			/* THIS IS THE STRUCTURE
+			{
+				"events": [
+					{
+						"replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+						"type": "message",
+						"timestamp": 1462629479859,
+						"source": {
+							"type": "user",
+							"userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+						},
+						"message": {
+							"id": "325708",
+							"type": "text",
+							"text": "Hello, world"
+						}
+					}
+				]
+			}
+			*/
+
+			// DO YOUR CODE HERE
+			
+		}
+
+	}
 }
--- application/models/Tebakkode_m.php
+++ application/models/Tebakkode_m.php
@@ -10,9 +10,73 @@ class Tebakkode_m extends CI_Model {
   function log_events($signature, $body)
   {
     $this->db->set('signature', $signature)
-    ->set('events', $body)
-    ->insert('eventlog');
+         ->set('events', $body)
+         ->insert('eventlog');
 
     return $this->db->insert_id();
   }
+
+  function getUser($userId)
+  {
+    $data = $this->db->where('user_id', $userId)->get('users')->row_array();
+
+    if(count($data) > 0)
+      return $data;
+
+    return false;
+  }
+
+  function saveUser($profile)
+  {
+    if($this->getUser($profile['userId']) !== false)
+      return false;
+
+    $this->db->set('user_id', $profile['userId'])
+         ->set('display_name', $profile['displayName'])
+         ->insert('users');
+
+    return $this->db->insert_id();
+  }
+
+  function getQuestion($questionNum)
+  {
+    $data = $this->db->where('number', $questionNum)
+           ->get('questions')
+           ->row_array();
+
+    if(count($data)>0)
+      return $data;
+
+    return false;
+  }
+
+  function isAnswerEqual($number, $answer)
+  {
+    $this->db->where('number', $number)
+         ->where('answer', $answer);
+
+    if(count($this->db->get('questions')->row()) > 0)
+      return true;
+
+    return false;
+  }
+
+  function setUserProgress($user_id, $newNumber)
+  {
+    $this->db->set('number', $newNumber)
+         ->where('user_id', $user_id)
+         ->update('users');
+
+    return $this->db->affected_rows();
+  }
+
+  function setScore($user_id, $score)
+  {
+    $this->db->set('score', $score)
+         ->where('user_id', $user_id)
+         ->update('users');
+
+    return $this->db->affected_rows();
+  }
+
 }
